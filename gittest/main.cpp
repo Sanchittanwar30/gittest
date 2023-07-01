d300d4f2d7d7adb5935c72f33c726ec46bc78a47
Sanchittanwar30 <111452909+Sanchittanwar30@users.noreply.github.com>
2023-07-02 04:03:45 +0530

    checking

--- gittest/main.cpp
+++ gittest/main.cpp
@@ -7,10 +7,12 @@
 
 #include <iostream>
 using namespace std;
-
+// random statement to check few things
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
     cout<<"hola amigo";
+    
+    cout<<"what is git ?";
     return 0;
 }
