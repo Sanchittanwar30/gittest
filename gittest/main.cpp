c919a76c6e89af1c9cbff1b1283d0beb77a0b4b2
sanchit <sanchit.tanwer@gmail.com>
2023-07-02 03:50:24 +0530

    Initial Commit

---
+++ gittest/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  gittest
+//
+//  Created by Sanchit Tanwer on 02/07/23.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    cout<<"hola amigo";
+    return 0;
+}
